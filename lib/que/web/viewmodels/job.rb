105675274bead906a41277791a8556e259aaa92a
Javier Cestau <javier.cestau1209@gmail.com>
2023-06-30 15:48:23 +0200

    Update job.rb

--- lib/que/web/viewmodels/job.rb
+++ lib/que/web/viewmodels/job.rb
@@ -2,7 +2,7 @@ module Que::Web::Viewmodels
   class Job < Struct.new(
       :priority, :run_at, :id, :job_class, :error_count, :last_error_message,
       :queue, :last_error_backtrace, :finished_at, :expired_at, :args, :data,
-      :backend_pid)
+      :kwargs, :backend_pid)
 
     def initialize(job)
       members.each do |m|
